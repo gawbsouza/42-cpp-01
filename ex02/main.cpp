585cdad77832aa868bf12b734d707d71c0b642a1
Gabriel Souza <email@gawbsouza.dev.br>
2024-01-20 12:27:37 -0300

    add ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gasouza <gasouza@student.42sp.org.br >     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 20:41:05 by gasouza           #+#    #+#             */
+/*   Updated: 2024/01/15 21:28:35 by gasouza          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int main()
+{
+    std::string myString = "HI THIS IS BRAIN";
+
+    std::string *stringPTR = &myString;
+    std::string &stringREF = myString;
+
+    std::cout << "The memory address of the string variable:" << std::endl;
+
+    std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+    
+    std::cout << std::endl;
+
+    std::cout << "The value of the string variable:" << std::endl;
+    
+    std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "The value pointed to by stringREF: " << stringREF << std::endl; 
+    
+    return 0;
+}
